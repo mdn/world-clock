bf2aeec4546e2f7cc1ad60b0cd15435592233319
Chris Mills <cmills@opera.com>
2015-01-26 10:28:19 +0000

    just putting dist/ on gh-pages branch

--- app/adapters/application.js
+++
@@ -1,5 +0,0 @@
-import LFAdapter from 'ember-localforage-adapter/adapters/localforage';
-
-export default LFAdapter.extend({
-    namespace: 'WorldTimeZones'
-});
--- app/routes/clock.js
+++
@@ -1,6 +0,0 @@
-import Ember from 'ember';
-export default Ember.Route.extend({
-    model: function() {
-        return this.get('store').find('timezone');
-    }
-});
